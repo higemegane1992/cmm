011496248a0d61387e6f2e55431a8d8e68a3d638
higemegane1992 <kazuaki.hirata.0905@gmail.com>
2020-09-13 15:59:28 +0900

    Create db/seeds.rb to insert machine data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,96 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Machine.create(
+  [
+    {
+      name: '1-1'
+    },
+    {
+      name: '1-2'
+    },
+    {
+      name: '1-3'
+    },
+    {
+      name: '2-1'
+    },
+    {
+      name: '2-2'
+    },
+    {
+      name: '2-3'
+    },
+    {
+      name: '3-1'
+    },
+    {
+      name: '3-2'
+    },
+    {
+      name: '3-3'
+    },
+    {
+      name: '4-1'
+    },
+    {
+      name: '4-2'
+    },
+    {
+      name: '4-3'
+    },
+    {
+      name: '5-1'
+    },
+    {
+      name: '5-2'
+    },
+    {
+      name: '5-3'
+    },
+    {
+      name: '6-1'
+    },
+    {
+      name: '6-2'
+    },
+    {
+      name: '6-3'
+    },
+    {
+      name: '7-1'
+    },
+    {
+      name: '7-2'
+    },
+    {
+      name: '7-3'
+    },
+    {
+      name: '8-1'
+    },
+    {
+      name: '8-2'
+    },
+    {
+      name: '8-3'
+    },
+    {
+      name: '9-1'
+    },
+    {
+      name: '9-2'
+    },
+    {
+      name: '9-3'
+    },
+    {
+      name: '10-1'
+    },
+    {
+      name: '10-2'
+    },
+    {
+      name: '10-3'
+    }
+  ]
+)
