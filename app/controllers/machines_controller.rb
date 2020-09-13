fec8541a7330dcc147a900dbd7d8780e74bac4a9
higemegane1992 <kazuaki.hirata.0905@gmail.com>
2020-09-13 17:20:58 +0900

    Create new page of machines

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -5,4 +5,19 @@ class MachinesController < ApplicationController
   def index
     @machines = Machine.all
   end
+
+  def new
+    @machine = Machine.new
+  end
+
+  def create
+    Machine.create(machine_params)
+    redirect_to machines_path, notice: '新しい生産ラインが登録されました'
+  end
+
+  private
+
+  def machine_params
+    params.require(:machine).permit(:name)
+  end
 end
