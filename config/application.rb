41a67f85a692dbd939c8d881874963a18e45a7e8
higemegane1992 <kazuaki.hirata.0905@gmail.com>
2020-09-13 14:06:27 +0900

    prevent to generate test framework

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,10 @@ module Cmm
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.generators do |g|
+      g.test_framework false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
