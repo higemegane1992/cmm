c38ca354731fbf893e0763c55bb2b790a53c92c1
higemegane1992 <kazuaki.hirata.0905@gmail.com>
2020-09-15 22:24:51 +0900

    Fix db/seeds.rb to insert mold data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,165 +1,17 @@
 # frozen_string_literal: true
 
-Machine.create(
-  [
-    {
-      name: '1-1'
-    },
-    {
-      name: '1-2'
-    },
-    {
-      name: '1-3'
-    },
-    {
-      name: '2-1'
-    },
-    {
-      name: '2-2'
-    },
-    {
-      name: '2-3'
-    },
-    {
-      name: '3-1'
-    },
-    {
-      name: '3-2'
-    },
-    {
-      name: '3-3'
-    },
-    {
-      name: '4-1'
-    },
-    {
-      name: '4-2'
-    },
-    {
-      name: '4-3'
-    },
-    {
-      name: '5-1'
-    },
-    {
-      name: '5-2'
-    },
-    {
-      name: '5-3'
-    },
-    {
-      name: '6-1'
-    },
-    {
-      name: '6-2'
-    },
-    {
-      name: '6-3'
-    },
-    {
-      name: '7-1'
-    },
-    {
-      name: '7-2'
-    },
-    {
-      name: '7-3'
-    },
-    {
-      name: '8-1'
-    },
-    {
-      name: '8-2'
-    },
-    {
-      name: '8-3'
-    },
-    {
-      name: '9-1'
-    },
-    {
-      name: '9-2'
-    },
-    {
-      name: '9-3'
-    },
-    {
-      name: '10-1'
-    },
-    {
-      name: '10-2'
-    },
-    {
-      name: '10-3'
-    }
-  ]
-)
+10.times do |i|
+  3.times do |j|
+    Machine.create(name: "#{i + 1}-#{j + 1}")
+  end
+end
 
-Product.create(
-  [
-    {
-      number: '000000-00000',
-      name: '00000'
-    },
-    {
-      number: '000000-00001',
-      name: '00001'
-    },
-    {
-      number: '000000-00002',
-      name: '00002'
-    },
-    {
-      number: '000000-00003',
-      name: '00003'
-    },
-    {
-      number: '000000-00004',
-      name: '00004'
-    },
-    {
-      number: '000000-00005',
-      name: '00005'
-    },
-    {
-      number: '000000-00006',
-      name: '00006'
-    },
-    {
-      number: '000000-00007',
-      name: '00007'
-    },
-    {
-      number: '000000-00008',
-      name: '00008'
-    },
-    {
-      number: '000000-00009',
-      name: '00009'
-    },
-    {
-      number: '000000-00010',
-      name: '00010'
-    },
-    {
-      number: '000000-00011',
-      name: '00011'
-    },
-    {
-      number: '000000-00012',
-      name: '00012'
-    },
-    {
-      number: '000000-00013',
-      name: '00013'
-    },
-    {
-      number: '000000-00014',
-      name: '00014'
-    },
-    {
-      number: '000000-00015',
-      name: '00015'
-    }
-  ]
-)
+15.times do |num|
+  Product.create(number: "000000-000#{sprintf("%02d", num + 1)}", name: "000#{sprintf("%02d", num + 1)}")
+end
+
+15.times do |i|
+  6.times do |j|
+    Mold.create(product_id: i + 1, number: j + 1)
+  end
+end
