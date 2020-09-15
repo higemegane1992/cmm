7c61cbe00543bbe36ca1f50ad13e5f8e75750eb3
higemegane1992 <kazuaki.hirata.0905@gmail.com>
2020-09-16 00:44:26 +0900

    Create new and show pages of molds

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'machines#index'
 
-  resources :products
+  resources :products do
+    resources :molds, only: %i[new create show destroy]
+  end
 
   resources :machines
 end
